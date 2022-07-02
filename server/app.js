7ab1263a952af0baa2a8f0151a1ad0a8bcdb1cff
Alp Tarla <alptarla@gmal.com>
2022-07-02 15:02:14 +0300

    implement eslint/prettier config

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
