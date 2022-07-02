7bab6396174de78a32d56e855bbc7155c2041080
Alp Tarla <alptarla@gmal.com>
2022-07-02 14:50:50 +0300

    init commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+
+module.exports = app;
